cac9e65bea3a690e973db90c56cf1a3dc24c48c4
Mr-Ayush <mr.ayush1125@gmail.com>
2023-10-27 20:50:01 +0530

    Error resolved

--- src/app/(root)/layout.js
+++ src/app/(root)/layout.js
@@ -1,9 +1,14 @@
 import DashboardNav from "@/components/navbar/DashboardNav";
+import { Suspense } from "react";
 
 const MainLayout = ({ children }) => {
   return (
     <main>
-      <DashboardNav />
+      <div className="w-full">
+        <Suspense fallback={<h2>Loading Nav..</h2>}>
+          <DashboardNav />
+        </Suspense>
+      </div>
       <div className="pt-10">{children}</div>
     </main>
   );
