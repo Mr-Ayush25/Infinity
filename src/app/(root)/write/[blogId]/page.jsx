1d980f950d290d92dc6297fa177c6fe3a907fd37
Mr-Ayush <mr.ayush1125@gmail.com>
2023-10-28 12:51:12 +0530

    error resolving

--- src/app/(root)/write/[blogId]/page.jsx
+++ src/app/(root)/write/[blogId]/page.jsx
@@ -7,12 +7,12 @@ const page = async ({ params }) => {
   const blog = await getBlog(blogId);
   return (
     <section className="min-h-screen pt-16">
-      <h2 className="text-center text-3xl pb-4 font-semibold">Edit blog</h2>
-      <div className="max-w-10xl mx-auto w-full max-md:mb-20">
-        <Suspense fallback={<p>Loading feed...</p>}>
+      <Suspense fallback={<p>Loading feed...</p>}>
+        <h2 className="text-center text-3xl pb-4 font-semibold">Edit blog</h2>
+        <div className="max-w-10xl mx-auto w-full max-md:mb-20">
           <DashboardContent blog={JSON.parse(JSON.stringify(blog))} />
-        </Suspense>
-      </div>
+        </div>
+      </Suspense>
     </section>
   );
 };
